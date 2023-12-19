8ccf8277ec1be8376fdb95172ec596b640b5730a
Venkat <venkatsaimane@gmail.com>
2023-12-19 20:03:17 +0530

    Header & Navbar updated

--- src/components/HeroSection/Hero.jsx
+++ src/components/HeroSection/Hero.jsx
@@ -4,12 +4,14 @@ import styles from "./Hero.module.css";
 export default function Hero({ props }) {
   return (
     <div className={styles.hero}>
-      <div>
-        <h1>100 Thousand Songs, ad-free</h1>
-        <h1>Over thousands podcast episodes</h1>
-      </div>
-      <div>
-        <img src={props} alt="HeadSet" />
+      <div className={styles.context}>
+        <div>
+          <h1>100 Thousand Songs, ad-free</h1>
+          <h1>Over thousands podcast episodes</h1>
+        </div>
+        <div>
+          <img src={props} alt="HeadSet" />
+        </div>
       </div>
     </div>
   );
