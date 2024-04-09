f599d74e285993ca33642d90dc52a70ff34de8a4
Venkat <venkatsaimane@gmail.com>
2024-04-09 20:12:53 +0530

    Added Feedback

--- src/components/NavBar/FeedBack/FeedBack.jsx
+++ src/components/NavBar/FeedBack/FeedBack.jsx
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import styles from "./FeedBack.module.css";
 
 function FeedBack({ setFeedBack }) {
+  const [state, setState] = useState(true);
   const [formData, setFormData] = useState({
     fullName: "",
     email: "",
@@ -26,7 +27,7 @@ function FeedBack({ setFeedBack }) {
       description: "",
     });
     console.log(formData);
-    setFeedBack(false);
+    setState(false);
   };
 
   return (
@@ -34,54 +35,58 @@ function FeedBack({ setFeedBack }) {
       <button className={styles.close} onClick={() => setFeedBack(false)}>
         x
       </button>
-      <form onSubmit={handleSubmit} className={styles.form}>
-        <input
-          type="text"
-          id="fullName"
-          name="fullName"
-          value={formData.fullName}
-          onChange={handleChange}
-          placeholder="Full Name"
-          required
-          className={styles.fields}
-        />
+      {state ? (
+        <form onSubmit={handleSubmit} className={styles.form}>
+          <input
+            type="text"
+            id="fullName"
+            name="fullName"
+            value={formData.fullName}
+            onChange={handleChange}
+            placeholder="Full Name"
+            required
+            className={styles.fields}
+          />
 
-        <input
-          type="email"
-          id="email"
-          name="email"
-          value={formData.email}
-          onChange={handleChange}
-          placeholder="Email ID"
-          required
-          className={styles.fields}
-        />
+          <input
+            type="email"
+            id="email"
+            name="email"
+            value={formData.email}
+            onChange={handleChange}
+            placeholder="Email ID"
+            required
+            className={styles.fields}
+          />
 
-        <input
-          type="text"
-          id="subject"
-          name="subject"
-          value={formData.subject}
-          onChange={handleChange}
-          placeholder="Subject"
-          required
-          className={styles.fields}
-        />
+          <input
+            type="text"
+            id="subject"
+            name="subject"
+            value={formData.subject}
+            onChange={handleChange}
+            placeholder="Subject"
+            required
+            className={styles.fields}
+          />
 
-        <textarea
-          id="description"
-          name="description"
-          value={formData.description}
-          onChange={handleChange}
-          placeholder="Description"
-          required
-          className={styles.fields}
-        />
+          <textarea
+            id="description"
+            name="description"
+            value={formData.description}
+            onChange={handleChange}
+            placeholder="Description"
+            required
+            className={styles.fields}
+          />
 
-        <button type="submit" className={styles.button}>
-          Submit
-        </button>
-      </form>
+          <button type="submit" className={styles.button}>
+            Submit
+          </button>
+        </form>
+      ) : (
+        <div>Thank you for your valued feedback!</div>
+      )}
     </div>
   );
 }
